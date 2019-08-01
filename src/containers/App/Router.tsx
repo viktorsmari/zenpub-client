40edc8063242e8b46105a50ce82f9ea022bbb693
ivan minutillo <bernini@inventati.org>
2019-08-01 16:24:15 +0200

    added discover new route

--- src/containers/App/Router.tsx
+++ src/containers/App/Router.tsx
@@ -6,49 +6,57 @@ import CommunitiesAll from '../../pages/communities.all/CommunitiesAll';
 import CollectionsAll from '../../pages/collections.all';
 import CommunitiesCommunity from '../../pages/communities.community/CommunitiesCommunity';
 import CollectionsCollection from '../../pages/collections.collection/CollectionsCollection';
-import Header from '../../components/header';
+// import Header from '../../components/header';
 import Login from '../../pages/login/Login';
 import NotFound from '../../pages/not-found/NotFound';
 import ProtectedRoute from './ProtectedRoute';
 import Search from '../../pages/search/Search';
 import Home from '../../pages/home';
+import Discover from '../../pages/discover';
 import Profile from '../../pages/Profile';
 import User from '../../pages/User';
 import Settings from '../../pages/settings';
-import media from 'styled-media-query';
+// import media from 'styled-media-query';
 import Reset from '../../pages/Reset';
 import CreateNewPassword from '../../pages/CreateNewPassword';
+
+
+import {Flex} from 'rebass'
+import Sidebar from '../../sections/sidebar/sidebarHOC'
+
+
 
 const AppInner = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
   height: 100%;
+  justify-content: center;
 `;
-const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  flex: 1;
-`;
+// const Main = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 100%;
+//   height: 100%;
+//   flex: 1;
+// `;
 
-const Overlay = styled.div`
-  position: absolute;
-  left: 240px;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.3);
-  z-index: 99999;
-  cursor: pointer;
+// const Overlay = styled.div`
+//   position: absolute;
+//   left: 240px;
+//   right: 0;
+//   top: 0;
+//   bottom: 0;
+//   background: rgba(0, 0, 0, 0.3);
+//   z-index: 99999;
+//   cursor: pointer;
 
-  ${media.greaterThan('medium')`
-  display: none;
-  `} &:hover {
-    background: rgba(0, 0, 0, 0.35);
-  }
-`;
+//   ${media.greaterThan('medium')`
+//   display: none;
+//   `} &:hover {
+//     background: rgba(0, 0, 0, 0.35);
+//   }
+// `;
 
 /**
  * The application routes definition.
@@ -69,18 +77,11 @@ export default compose(withState('sidebar', 'onSidebar', false))(p => (
           path="/"
           component={props => (
             <>
-              {/* <Nav sidebar={p.sidebar} /> */}
-              <Main>
-                {p.sidebar ? (
-                  <Overlay onClick={() => p.onSidebar(false)} />
-                ) : null}
-                <Header
-                  onSidebar={p.onSidebar}
-                  history={props.history}
-                  sidebar={p.sidebar}
-                />
+              <Flex justifyContent={"center"}>
+                <Sidebar />
                 <Switch>
                   <Route exact path="/" component={Home} />
+                  <Route exact path="/discover" component={Discover} />
                   <Route exact path="/settings" component={Settings} />
                   <Route exact path="/communities" component={CommunitiesAll} />
                   <Route
@@ -96,7 +97,7 @@ export default compose(withState('sidebar', 'onSidebar', false))(p => (
                   <Route exact path="/user/:id" component={User} />
                   <Route component={NotFound} />
                 </Switch>
-              </Main>
+              </Flex>
             </>
           )}
         />
