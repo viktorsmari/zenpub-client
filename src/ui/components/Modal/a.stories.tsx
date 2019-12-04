a78e3e73a70598fdec196885d2f646a54025968e
aleclofabbro <alessandro.giansanti@gmail.com>
2019-12-04 11:13:08 +0100

    moving some into ui

--- src/ui/components/Modal/a.stories.tsx
+++ src/ui/components/Modal/a.stories.tsx
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Modal from '.';
 
-storiesOf('FC/Skel', module).add('Simple', () => (
+storiesOf('Components/Modal', module).add('Simple', () => (
   <Modal closeModal={action('close modal')}>Hello Modal</Modal>
 ));
