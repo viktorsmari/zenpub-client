4f406c3568b49b9a6a149437e913c649f6de83f5
aleclofabbro <alessandro.giansanti@gmail.com>
2019-12-18 13:42:06 +0100

    show featured*

--- src/components/featuredCollections/index.tsx
+++ src/components/featuredCollections/index.tsx
@@ -1,134 +1,93 @@
 import { Trans } from '@lingui/macro';
-import { FetchResult } from 'apollo-link';
-import { GetFeaturedCollectionsQueryResult } from 'graphql/generated/getFeaturedCollections.generated';
-import React from 'react';
-import { graphql, OperationOption } from 'react-apollo';
+import { useGetFeaturedCollectionsQuery } from 'graphql/generated/getFeaturedCollections.generated';
+import React, { useRef } from 'react';
 import Slider from 'react-slick';
-import { compose } from 'recompose';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
-import { GRAPHQL_ENDPOINT } from '../../constants';
 import CollectionSmall from '../elements/Collection/CollectionSmall';
 import { ChevronLeft, Right } from '../elements/Icons';
 import Loader from '../elements/Loader/Loader';
 import { RightContext, Title } from '../featuredCommunities';
-const getFeaturedCollections = require('../../graphql/getFeaturedCollections.graphql');
 
-interface Props {
-  data: FetchResult<GetFeaturedCollectionsQueryResult>['data'];
-}
+const MultipleItems: React.FC = () => {
+  const props = useGetFeaturedCollectionsQuery();
+  const sliderRef = useRef<Slider>();
 
-class MultipleItems extends React.Component<Props> {
-  private slider: any;
-
-  constructor(props) {
-    super(props);
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-  }
-  next() {
-    this.slider.slickNext();
-  }
-  previous() {
-    this.slider.slickPrev();
-  }
-  render() {
-    const settings = {
-      dots: false,
-      arrows: false,
-      infinite: true,
-      autoplay: false,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
-    return (
-      <>
-        <Title>
-          <h5>
-            <Trans>Featured collections</Trans>{' '}
-          </h5>
-          <RightContext>
-            <span onClick={this.previous}>
-              <ChevronLeft
-                width={26}
-                height={26}
-                strokeWidth={1}
-                color={'inherit'}
-              />
-            </span>
-            <span onClick={this.next}>
-              <Right width={26} height={26} strokeWidth={1} color={'inherit'} />
-            </span>
-          </RightContext>
-        </Title>
-        {!this.props.data || !this.props.data.data || this.props.data.error ? (
-          <span>
-            <Trans>{/* Error loading featured collections */}</Trans>
+  return (
+    <>
+      <Title>
+        <h5>
+          <Trans>Featured collections</Trans>{' '}
+        </h5>
+        <RightContext>
+          <span onClick={sliderRef.current && sliderRef.current.slickPrev}>
+            <ChevronLeft
+              width={26}
+              height={26}
+              strokeWidth={1}
+              color={'inherit'}
+            />
           </span>
-        ) : this.props.data.loading ? (
-          <Loader />
-        ) : (
-          <Slider ref={c => (this.slider = c)} {...settings}>
-            {this.props.data.data.instance
-              ? this.props.data.data.instance.featuredCollections.edges.map(
-                  edge =>
-                    edge &&
-                    edge.node.context.__typename === 'Collection' && (
-                      <CollectionSmall collection={edge.node.context} />
-                    )
-                )
-              : null}
-          </Slider>
-        )}
-      </>
-    );
-  }
-}
+          <span onClick={sliderRef.current && sliderRef.current.slickNext}>
+            <Right width={26} height={26} strokeWidth={1} color={'inherit'} />
+          </span>
+        </RightContext>
+      </Title>
+      {!props.data || !props.data.instance || props.error ? (
+        <span>
+          <Trans>{/* Error loading featured collections */}</Trans>
+        </span>
+      ) : props.loading ? (
+        <Loader />
+      ) : (
+        <Slider
+          ref={c => (sliderRef.current = c || undefined)}
+          {...sliderSettings}
+        >
+          {props.data.instance.featuredCollections.edges.map(
+            edge =>
+              edge &&
+              edge.node.context.__typename === 'Collection' && (
+                <CollectionSmall collection={edge.node.context} />
+              )
+          )}
+        </Slider>
+      )}
+    </>
+  );
+};
 
-const is_home =
-  GRAPHQL_ENDPOINT == 'https://home.moodle.net/api/graphql' ? true : false;
+export default MultipleItems;
 
-const withGetInbox = graphql<
-  {},
-  {
-    data: any;
-  }
->(getFeaturedCollections, {
-  options: {
-    variables: {
-      one: is_home ? 'cd514675-e822-4041-84e8-a5493e57d7d1' : null,
-      two: is_home ? '0e745426-995f-4755-80a0-59df867fd6ab' : null
-      // three: IS_DEV ? '5487' : '5571',
-      // four: IS_DEV ? '8092' : '5487',
-      // five: IS_DEV ? '690' : '4944',
-      // six: IS_DEV ? '3790' : '2374',
-      // seven: IS_DEV ? '4848' : '5571'
+const sliderSettings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplay: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
     }
-  }
-}) as OperationOption<{}, {}>;
-
-export default compose(withGetInbox)(MultipleItems);
+  ]
+};
