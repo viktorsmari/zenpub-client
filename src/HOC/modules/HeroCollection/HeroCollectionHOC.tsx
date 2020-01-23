911ecc2f287fb8fe2f9237ad51f31e330acb2f5f
aleclofabbro <alessandro.giansanti@gmail.com>
2020-01-23 16:16:24 +0100

    collection hero ok

---
+++ src/HOC/modules/HeroCollection/HeroCollectionHOC.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { SessionContext } from 'context/global/sessionCtx';
+import { useDeleteMutationMutation } from 'graphql/delete.generated';
+import { useFollowMutationMutation } from 'graphql/follow.generated';
+import { SFC, useContext, useMemo } from 'react';
+import HeroCollection, {
+  Props as HeroProps,
+  Status
+} from 'ui/modules/HeroCollection';
+import { Collection } from 'graphql/types.generated';
+import { EditCollectionPanelHOC } from 'HOC/modules/EditCollectionPanel/editCollectionPanelHOC';
+import { useHeroCollectionQuery } from './HeroCollection.generated';
+
+export interface Props {
+  collectionId: Collection['id'];
+}
+export const HeroCollectionHOC: SFC<Props> = ({ collectionId }) => {
+  const session = useContext(SessionContext);
+  const [joinMutation, joinMutationStatus] = useFollowMutationMutation();
+  const [unjoinMutation, unjoinMutationStatus] = useDeleteMutationMutation();
+  const collectionQuery = useHeroCollectionQuery({
+    variables: { collectionId }
+  });
+  const heroProps = useMemo<HeroProps>(
+    () => {
+      if (
+        collectionQuery.loading ||
+        collectionQuery.error ||
+        !collectionQuery.data ||
+        !collectionQuery.data.collection
+      ) {
+        return {
+          collection: {
+            status: Status.Loading
+          }
+        };
+      }
+      const collection = collectionQuery.data.collection;
+      const props: HeroProps = {
+        collection: {
+          status: Status.Loaded,
+          //FIXME https://gitlab.com/moodlenet/meta/issues/185
+          isMine: !session.me || session.me.user.id === collection.creator!.id,
+          following: !!collection.myFollow,
+          icon: collection.icon || '',
+          title: collection.name,
+          preferredUsername: collection.preferredUsername,
+          //FIXME https://gitlab.com/moodlenet/meta/issues/185
+          summary: collection.summary || '',
+          toggleJoin: {
+            toggle: () =>
+              collection.myFollow
+                ? unjoinMutation({
+                    variables: { contextId: collection.myFollow.id }
+                  })
+                : joinMutation({ variables: { contextId: collection.id } }),
+            isSubmitting: collection.myFollow
+              ? unjoinMutationStatus.loading
+              : joinMutationStatus.loading
+          },
+          EditCollectionPanel: ({ done }) => (
+            <EditCollectionPanelHOC done={done} collectionId={collection.id} />
+          )
+        }
+      };
+      return props;
+    },
+    [
+      collectionQuery,
+      session.me,
+      joinMutation,
+      joinMutationStatus,
+      unjoinMutation,
+      unjoinMutationStatus
+    ]
+  );
+  return <HeroCollection {...heroProps} />;
+};
