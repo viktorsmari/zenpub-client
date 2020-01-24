76b84c70d8a3ca721b740d09718d1dc9add991f6
aleclofabbro <alessandro.giansanti@gmail.com>
2020-01-24 17:53:07 +0100

    fixed collectionPageHOC after merge

--- src/HOC/pages/collection/CollectionPage.generated.tsx
+++ src/HOC/pages/collection/CollectionPage.generated.tsx
@@ -1,6 +1,6 @@
 import * as Types from '../../../graphql/types.generated';
 
-import { ActivityPreviewDataFragment } from '../../modules/ActivityPreview/getActivityPreview.generated';
+import { ActivityPreviewDataFragment, ActivityPreviewResourceCtxFragment, ActivityPreviewLikeCtxFragment, ActivityPreviewCommentCtxBaseFragment, ActivityPreviewResourceCtxFragmentDoc, ActivityPreviewLikeCtxFragmentDoc, ActivityPreviewCommentCtxBaseFragmentDoc } from '../../modules/ActivityPreview/getActivityPreview.generated';
 import { HeroCollectionDataFragment } from '../../modules/HeroCollection/HeroCollection.generated';
 import gql from 'graphql-tag';
 import { HeroCollectionDataFragmentDoc } from '../../modules/HeroCollection/HeroCollection.generated';
@@ -23,7 +23,18 @@ export type CollectionPageQuery = (
   { __typename: 'RootQueryType' }
   & { collection: Types.Maybe<(
     { __typename: 'Collection' }
-    & { outbox: Types.Maybe<(
+    & Pick<Types.Collection, 'resourceCount'>
+    & { resources: Types.Maybe<(
+      { __typename: 'ResourcesEdges' }
+      & { edges: Array<Types.Maybe<(
+        { __typename: 'ResourcesEdge' }
+        & { node: (
+          { __typename: 'Resource' }
+          & Pick<Types.Resource, 'createdAt'>
+          & CollectionPageResourceFragment
+        ) }
+      )>> }
+    )>, outbox: Types.Maybe<(
       { __typename: 'ActivitiesEdges' }
       & { edges: Types.Maybe<Array<Types.Maybe<(
         { __typename: 'ActivitiesEdge' }
@@ -37,11 +48,69 @@ export type CollectionPageQuery = (
   )> }
 );
 
+export type CollectionPageResourceFragment = (
+  { __typename: 'Resource' }
+  & Pick<Types.Resource, 'createdAt'>
+  & ActivityPreviewResourceCtxFragment
+);
 
+export type CollectionPageResourceLikeMutationVariables = {
+  contextId: Types.Scalars['String']
+};
+
+
+export type CollectionPageResourceLikeMutation = (
+  { __typename: 'RootMutationType' }
+  & { createLike: Types.Maybe<(
+    { __typename: 'Like' }
+    & ActivityPreviewLikeCtxFragment
+  )> }
+);
+
+export type CollectionPageResourceUnlikeMutationVariables = {
+  contextId: Types.Scalars['String']
+};
+
+
+export type CollectionPageResourceUnlikeMutation = (
+  { __typename: 'RootMutationType' }
+  & { delete: Types.Maybe<{ __typename: 'Collection' } | { __typename: 'Comment' } | { __typename: 'Community' } | { __typename: 'Feature' } | { __typename: 'Flag' } | { __typename: 'Follow' } | { __typename: 'Like' } | { __typename: 'Resource' } | { __typename: 'Thread' } | { __typename: 'User' }> }
+);
+
+export type CollectionPageResourceCreateReplyMutationVariables = {
+  comment: Types.CommentInput,
+  inReplyToId: Types.Scalars['String'],
+  threadId: Types.Scalars['String']
+};
+
+
+export type CollectionPageResourceCreateReplyMutation = (
+  { __typename: 'RootMutationType' }
+  & { createReply: Types.Maybe<(
+    { __typename: 'Comment' }
+    & ActivityPreviewCommentCtxBaseFragment
+  )> }
+);
+
+export const CollectionPageResourceFragmentDoc = gql`
+    fragment CollectionPageResource on Resource {
+  ...ActivityPreviewResourceCtx
+  createdAt
+}
+    ${ActivityPreviewResourceCtxFragmentDoc}`;
 export const CollectionPageDocument = gql`
     query collectionPage($collectionId: String!) {
   collection(collectionId: $collectionId) {
     ...HeroCollectionData
+    resourceCount
+    resources {
+      edges {
+        node {
+          ...CollectionPageResource
+          createdAt
+        }
+      }
+    }
     outbox {
       edges {
         node {
@@ -52,6 +121,7 @@ export const CollectionPageDocument = gql`
   }
 }
     ${HeroCollectionDataFragmentDoc}
+${CollectionPageResourceFragmentDoc}
 ${ActivityPreviewDataFragmentDoc}`;
 export type CollectionPageComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<CollectionPageQuery, CollectionPageQueryVariables>, 'query'> & ({ variables: CollectionPageQueryVariables; skip?: boolean; } | { skip: boolean; });
 
@@ -96,6 +166,155 @@ export function useCollectionPageLazyQuery(baseOptions?: ApolloReactHooks.LazyQu
 export type CollectionPageQueryHookResult = ReturnType<typeof useCollectionPageQuery>;
 export type CollectionPageLazyQueryHookResult = ReturnType<typeof useCollectionPageLazyQuery>;
 export type CollectionPageQueryResult = ApolloReactCommon.QueryResult<CollectionPageQuery, CollectionPageQueryVariables>;
+export const CollectionPageResourceLikeDocument = gql`
+    mutation collectionPageResourceLike($contextId: String!) {
+  createLike(contextId: $contextId) {
+    ...ActivityPreviewLikeCtx
+  }
+}
+    ${ActivityPreviewLikeCtxFragmentDoc}`;
+export type CollectionPageResourceLikeMutationFn = ApolloReactCommon.MutationFunction<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables>;
+export type CollectionPageResourceLikeComponentProps = Omit<ApolloReactComponents.MutationComponentOptions<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables>, 'mutation'>;
+
+    export const CollectionPageResourceLikeComponent = (props: CollectionPageResourceLikeComponentProps) => (
+      <ApolloReactComponents.Mutation<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables> mutation={CollectionPageResourceLikeDocument} {...props} />
+    );
+    
+export type CollectionPageResourceLikeProps<TChildProps = {}> = ApolloReactHoc.MutateProps<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables> & TChildProps;
+export function withCollectionPageResourceLike<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  CollectionPageResourceLikeMutation,
+  CollectionPageResourceLikeMutationVariables,
+  CollectionPageResourceLikeProps<TChildProps>>) {
+    return ApolloReactHoc.withMutation<TProps, CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables, CollectionPageResourceLikeProps<TChildProps>>(CollectionPageResourceLikeDocument, {
+      alias: 'collectionPageResourceLike',
+      ...operationOptions
+    });
+};
+
+/**
+ * __useCollectionPageResourceLikeMutation__
+ *
+ * To run a mutation, you first call `useCollectionPageResourceLikeMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCollectionPageResourceLikeMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [collectionPageResourceLikeMutation, { data, loading, error }] = useCollectionPageResourceLikeMutation({
+ *   variables: {
+ *      contextId: // value for 'contextId'
+ *   },
+ * });
+ */
+export function useCollectionPageResourceLikeMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables>) {
+        return ApolloReactHooks.useMutation<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables>(CollectionPageResourceLikeDocument, baseOptions);
+      }
+export type CollectionPageResourceLikeMutationHookResult = ReturnType<typeof useCollectionPageResourceLikeMutation>;
+export type CollectionPageResourceLikeMutationResult = ApolloReactCommon.MutationResult<CollectionPageResourceLikeMutation>;
+export type CollectionPageResourceLikeMutationOptions = ApolloReactCommon.BaseMutationOptions<CollectionPageResourceLikeMutation, CollectionPageResourceLikeMutationVariables>;
+export const CollectionPageResourceUnlikeDocument = gql`
+    mutation collectionPageResourceUnlike($contextId: String!) {
+  delete(contextId: $contextId) {
+    __typename
+  }
+}
+    `;
+export type CollectionPageResourceUnlikeMutationFn = ApolloReactCommon.MutationFunction<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables>;
+export type CollectionPageResourceUnlikeComponentProps = Omit<ApolloReactComponents.MutationComponentOptions<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables>, 'mutation'>;
+
+    export const CollectionPageResourceUnlikeComponent = (props: CollectionPageResourceUnlikeComponentProps) => (
+      <ApolloReactComponents.Mutation<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables> mutation={CollectionPageResourceUnlikeDocument} {...props} />
+    );
+    
+export type CollectionPageResourceUnlikeProps<TChildProps = {}> = ApolloReactHoc.MutateProps<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables> & TChildProps;
+export function withCollectionPageResourceUnlike<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  CollectionPageResourceUnlikeMutation,
+  CollectionPageResourceUnlikeMutationVariables,
+  CollectionPageResourceUnlikeProps<TChildProps>>) {
+    return ApolloReactHoc.withMutation<TProps, CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables, CollectionPageResourceUnlikeProps<TChildProps>>(CollectionPageResourceUnlikeDocument, {
+      alias: 'collectionPageResourceUnlike',
+      ...operationOptions
+    });
+};
+
+/**
+ * __useCollectionPageResourceUnlikeMutation__
+ *
+ * To run a mutation, you first call `useCollectionPageResourceUnlikeMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCollectionPageResourceUnlikeMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [collectionPageResourceUnlikeMutation, { data, loading, error }] = useCollectionPageResourceUnlikeMutation({
+ *   variables: {
+ *      contextId: // value for 'contextId'
+ *   },
+ * });
+ */
+export function useCollectionPageResourceUnlikeMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables>) {
+        return ApolloReactHooks.useMutation<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables>(CollectionPageResourceUnlikeDocument, baseOptions);
+      }
+export type CollectionPageResourceUnlikeMutationHookResult = ReturnType<typeof useCollectionPageResourceUnlikeMutation>;
+export type CollectionPageResourceUnlikeMutationResult = ApolloReactCommon.MutationResult<CollectionPageResourceUnlikeMutation>;
+export type CollectionPageResourceUnlikeMutationOptions = ApolloReactCommon.BaseMutationOptions<CollectionPageResourceUnlikeMutation, CollectionPageResourceUnlikeMutationVariables>;
+export const CollectionPageResourceCreateReplyDocument = gql`
+    mutation collectionPageResourceCreateReply($comment: CommentInput!, $inReplyToId: String!, $threadId: String!) {
+  createReply(comment: $comment, inReplyToId: $inReplyToId, threadId: $threadId) {
+    ...ActivityPreviewCommentCtxBase
+  }
+}
+    ${ActivityPreviewCommentCtxBaseFragmentDoc}`;
+export type CollectionPageResourceCreateReplyMutationFn = ApolloReactCommon.MutationFunction<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables>;
+export type CollectionPageResourceCreateReplyComponentProps = Omit<ApolloReactComponents.MutationComponentOptions<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables>, 'mutation'>;
+
+    export const CollectionPageResourceCreateReplyComponent = (props: CollectionPageResourceCreateReplyComponentProps) => (
+      <ApolloReactComponents.Mutation<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables> mutation={CollectionPageResourceCreateReplyDocument} {...props} />
+    );
+    
+export type CollectionPageResourceCreateReplyProps<TChildProps = {}> = ApolloReactHoc.MutateProps<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables> & TChildProps;
+export function withCollectionPageResourceCreateReply<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  CollectionPageResourceCreateReplyMutation,
+  CollectionPageResourceCreateReplyMutationVariables,
+  CollectionPageResourceCreateReplyProps<TChildProps>>) {
+    return ApolloReactHoc.withMutation<TProps, CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables, CollectionPageResourceCreateReplyProps<TChildProps>>(CollectionPageResourceCreateReplyDocument, {
+      alias: 'collectionPageResourceCreateReply',
+      ...operationOptions
+    });
+};
+
+/**
+ * __useCollectionPageResourceCreateReplyMutation__
+ *
+ * To run a mutation, you first call `useCollectionPageResourceCreateReplyMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCollectionPageResourceCreateReplyMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [collectionPageResourceCreateReplyMutation, { data, loading, error }] = useCollectionPageResourceCreateReplyMutation({
+ *   variables: {
+ *      comment: // value for 'comment'
+ *      inReplyToId: // value for 'inReplyToId'
+ *      threadId: // value for 'threadId'
+ *   },
+ * });
+ */
+export function useCollectionPageResourceCreateReplyMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables>) {
+        return ApolloReactHooks.useMutation<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables>(CollectionPageResourceCreateReplyDocument, baseOptions);
+      }
+export type CollectionPageResourceCreateReplyMutationHookResult = ReturnType<typeof useCollectionPageResourceCreateReplyMutation>;
+export type CollectionPageResourceCreateReplyMutationResult = ApolloReactCommon.MutationResult<CollectionPageResourceCreateReplyMutation>;
+export type CollectionPageResourceCreateReplyMutationOptions = ApolloReactCommon.BaseMutationOptions<CollectionPageResourceCreateReplyMutation, CollectionPageResourceCreateReplyMutationVariables>;
 
 
 export interface CollectionPageQueryOperation {
@@ -103,4 +322,28 @@ export interface CollectionPageQueryOperation {
   result: CollectionPageQuery
   variables: CollectionPageQueryVariables
   type: 'query'
+}
+
+
+export interface CollectionPageResourceLikeMutationOperation {
+  operationName: 'collectionPageResourceLike'
+  result: CollectionPageResourceLikeMutation
+  variables: CollectionPageResourceLikeMutationVariables
+  type: 'mutation'
+}
+
+
+export interface CollectionPageResourceUnlikeMutationOperation {
+  operationName: 'collectionPageResourceUnlike'
+  result: CollectionPageResourceUnlikeMutation
+  variables: CollectionPageResourceUnlikeMutationVariables
+  type: 'mutation'
+}
+
+
+export interface CollectionPageResourceCreateReplyMutationOperation {
+  operationName: 'collectionPageResourceCreateReply'
+  result: CollectionPageResourceCreateReplyMutation
+  variables: CollectionPageResourceCreateReplyMutationVariables
+  type: 'mutation'
 }
--- src/HOC/pages/collection/CollectionPageHOC.tsx
+++ src/HOC/pages/collection/CollectionPageHOC.tsx
@@ -1,11 +1,28 @@
-import React, { SFC, useMemo } from 'react';
+import { useFormik } from 'formik';
 import { Collection } from 'graphql/types.generated';
+import {
+  ActivityPreviewHOC,
+  getActions,
+  getActor
+} from 'HOC/modules/ActivityPreview/activityPreviewHOC';
+import { HeroCollectionHOC } from 'HOC/modules/HeroCollection/HeroCollectionHOC';
+import React, { SFC, useMemo } from 'react';
+import {
+  ActivityPreview,
+  Props as ActivityPreviewProps,
+  Status as ActivityPreviewStatus
+} from 'ui/modules/ActivityPreview';
+import * as UIP from 'ui/modules/ActivityPreview/preview';
 import CollectionPage, {
   Props as CollectionPageProps
 } from 'ui/pages/collection';
-import { useCollectionPageQuery } from './CollectionPage.generated';
-import { ActivityPreviewHOC } from 'HOC/modules/ActivityPreview/activityPreviewHOC';
-import { HeroCollectionHOC } from 'HOC/modules/HeroCollection/HeroCollectionHOC';
+import {
+  CollectionPageResourceFragment,
+  useCollectionPageQuery,
+  useCollectionPageResourceCreateReplyMutation,
+  useCollectionPageResourceLikeMutation,
+  useCollectionPageResourceUnlikeMutation
+} from './CollectionPage.generated';
 
 export interface Props {
   collectionId: Collection['id'];
@@ -20,7 +37,9 @@ export const CollectionPageHOC: SFC<Props> = ({ collectionId }) => {
         !collectionQ.data ||
         !collectionQ.data.collection ||
         !collectionQ.data.collection.outbox ||
-        !collectionQ.data.collection.outbox.edges
+        !collectionQ.data.collection.outbox.edges ||
+        !collectionQ.data.collection.resources ||
+        !collectionQ.data.collection.resources.edges
       ) {
         return null;
       }
@@ -38,8 +57,15 @@ export const CollectionPageHOC: SFC<Props> = ({ collectionId }) => {
       const HeroCollectionBox = (
         <HeroCollectionHOC collectionId={collectionId} />
       );
-      const resourcesEdges = collectionQ.data.collection;
-      const ResourceBoxes = 1;
+      const ResourceBoxes = collectionQ.data.collection.resources.edges
+        .map(edge => {
+          if (!edge || !edge.node) {
+            return null;
+          }
+          const resource = edge.node;
+          return <ResourceActivity resource={resource} key={resource.id} />;
+        })
+        .filter((_): _ is JSX.Element => !!_);
       const props: CollectionPageProps = {
         ActivityBoxes,
         HeroCollectionBox,
@@ -51,4 +77,71 @@ export const CollectionPageHOC: SFC<Props> = ({ collectionId }) => {
     [collectionQ]
   );
   return collectionPageProps && <CollectionPage {...collectionPageProps} />;
+};
+
+const ResourceActivity: SFC<{ resource: CollectionPageResourceFragment }> = ({
+  resource
+}) => {
+  if (!resource.creator) {
+    return null;
+  }
+  const [likeMut, likeMutStatus] = useCollectionPageResourceLikeMutation();
+  const [
+    unlikeMut,
+    unlikeMutStatus
+  ] = useCollectionPageResourceUnlikeMutation();
+  const [
+    createReplyMut,
+    createReplyMutStatus
+  ] = useCollectionPageResourceCreateReplyMutation();
+
+  const commentResourceFormik = useFormik<{ replyMessage: string }>({
+    initialValues: { replyMessage: '' },
+    onSubmit: ({ replyMessage }) => {
+      if (createReplyMutStatus.loading) {
+        return;
+      }
+      return createReplyMut({
+        variables: {
+          threadId: resource.id,
+          inReplyToId: resource.id,
+          comment: { content: replyMessage }
+        }
+      });
+    }
+  });
+  const toggleLikeFormik = useFormik<{}>({
+    initialValues: {},
+    onSubmit: () => {
+      if (likeMutStatus.loading || unlikeMutStatus.loading) {
+        return;
+      }
+      const { myLike } = resource;
+      if (myLike) {
+        return unlikeMut({ variables: { contextId: myLike.id } });
+      } else {
+        return likeMut({
+          variables: {
+            contextId: resource.id
+          }
+        });
+      }
+    }
+  });
+
+  const props: ActivityPreviewProps = {
+    actor: getActor(resource.creator),
+    context: {
+      type: UIP.ContextType.Resource,
+      link: `/thread/${resource.id}`,
+      verb: UIP.ContextVerb.Created,
+      title: resource.name,
+      icon: resource.icon || ''
+    },
+    createdAt: resource.createdAt,
+    status: ActivityPreviewStatus.Loaded,
+    actions: getActions(resource, commentResourceFormik, toggleLikeFormik)
+  };
+
+  return <ActivityPreview {...props} />;
 };
