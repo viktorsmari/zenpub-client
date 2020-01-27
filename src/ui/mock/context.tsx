3714c6853abe4a3222a1d0f71e5c53bda2997e2a
ivan minutillo <bernini@inventati.org>
2020-01-27 10:37:17 +0100

    hero collection includes the community

--- src/ui/mock/context.tsx
+++ src/ui/mock/context.tsx
@@ -89,6 +89,9 @@ export const getHeroCollectionProps = (): HeroCollectionProps => {
       icon: 'https://picsum.photos/800/300',
       title: 'Favourite books',
       preferredUsername: 'favbooks',
+      communityIcon: 'https://picsum.photos/800/300',
+      communityId: '2',
+      communityName: 'Super community',
       summary:
         'Cooperation combined with network effects is more effective than capitalist competition',
       toggleJoin: {
