af544a6ef0f38263778ca4e27f272f266a0cd906
aleclofabbro <alessandro.giansanti@gmail.com>
2020-02-06 13:21:19 +0100

    working, not tested for main integration as BE mutation crashes

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -14,6 +14,11 @@ import { Trans } from '@lingui/macro';
 // import { LocaleContext } from '../../containers/App/App';
 import styled from '../../themes/styled';
 import { Nav, Panel, PanelTitle, WrapperPanel } from 'ui/elements/Panel';
+import {
+  SearchHostIndexAndMyFollowingsQuery,
+  useSearchHostIndexAndMyFollowingsQuery
+} from './SearchData.generated';
+import { Hit } from './Hits';
 const urlParams = new URLSearchParams(window.location.search);
 const moodle_core_download_url = decodeURI(
   urlParams.get('moodle_core_download_url') || ''
@@ -40,8 +45,11 @@ const PagFlex = styled(Flex)`
   }
 `;
 
-function Result(props) {
-  var hit = props.hit;
+interface Result {
+  hit: Hit;
+  myInfo: SearchHostIndexAndMyFollowingsQuery;
+}
+const Result: React.SFC<Result> = ({ hit, myInfo }) => {
   return (
     <WrapperResult p={3}>
       {hit.collection ? (
@@ -59,40 +67,30 @@ function Result(props) {
         <span />
       )}
       <Preview
-        icon={hit.icon || hit.image}
-        title={hit.name}
-        summary={hit.summary}
-        url={hit.url || hit.canonicalUrl}
-        type={hit.index_type}
-        coreIntegrationURL={
-          moodle_core_download_url
-            ? moodle_core_download_url +
-              `&sourceurl=` +
-              encodeURIComponent(hit.url) +
-              `&moodleneturl=` +
-              encodeURIComponent(hit.canonicalUrl) +
-              `&name=` +
-              encodeURIComponent(hit.name) +
-              `&description=` +
-              encodeURIComponent(hit.summary)
-            : null
-        }
+        hit={hit}
+        myInfo={myInfo}
+        moodle_core_download_url={moodle_core_download_url}
       />
     </WrapperResult>
   );
-}
+};
 
-const InfiniteHits = ({ hits }) => (
+const InfiniteHits = ({ hits }: { hits: Hit[] }) => {
+  const { data } = useSearchHostIndexAndMyFollowingsQuery();
   // return the DOM output
-  <>
-    {hits.map(hit => (
-      <Result key={hit.objectID} hit={hit} />
-    ))}
-    <PagFlex alignItems="center" p={3}>
-      <Pagination showNext />
-    </PagFlex>
-  </>
-);
+  return data ? (
+    <>
+      {hits.map(hit => (
+        <Result key={hit.objectID} hit={hit} myInfo={data} />
+      ))}
+      <PagFlex alignItems="center" p={3}>
+        <Pagination showNext />
+      </PagFlex>
+    </>
+  ) : (
+    <div />
+  );
+};
 
 const CustomInfiniteHits = connectInfiniteHits(InfiniteHits);
 
---
+++ src/pages/search/lib.ts
@@ -0,0 +1,153 @@
+import {
+  SearchHostIndexAndMyFollowingsQuery,
+  SearchFollowedCollectionFragment,
+  SearchFollowedCommunityFragment,
+  useSearchFollowLocalMutation,
+  useSearchFollowRemoteMutation,
+  useSearchUnfollowMutation
+} from './SearchData.generated';
+import { Hit } from './Hits';
+import { useMemo, useCallback } from 'react';
+
+type Q = SearchHostIndexAndMyFollowingsQuery;
+export const useHit = (info: Q, hit: Hit) => {
+  const [followLocal, followLocalResult] = useSearchFollowLocalMutation();
+  const [followRemote, followRemoteResult] = useSearchFollowRemoteMutation();
+  const [unfollowHit, unfollowResult] = useSearchUnfollowMutation();
+
+  const mutating =
+    followLocalResult.loading ||
+    followRemoteResult.loading ||
+    unfollowResult.loading;
+  const followingCollections = useMemo(() => getFollowingCollections(info), [
+    info
+  ]);
+  const followingCommunities = useMemo(() => getFollowingCommunities(info), [
+    info
+  ]);
+
+  const isLocal = isHitLocal(hit, info);
+
+  const followContextId =
+    isLocal === null ? null : hitFollowContextId(hit, isLocal);
+  const isFollowable = !!followContextId;
+
+  const followId = useMemo(
+    () => hitFollowId(hit, followingCommunities, followingCollections),
+    [followingCollections, followingCommunities, hit]
+  );
+  const isFollowing = !!followId;
+
+  const follow = useCallback(
+    () => {
+      const followCtxId = followContextId;
+      if (mutating || !followCtxId) {
+        return;
+      }
+      if (isLocal) {
+        followLocal({ variables: { contextId: followCtxId } });
+      } else {
+        followRemote({ variables: { url: followCtxId } });
+      }
+    },
+    [followLocal, followRemote, isLocal, hit, followContextId]
+  );
+
+  const unfollow = useCallback(
+    () => {
+      if (mutating || !followId) {
+        return;
+      }
+      unfollowHit({ variables: { contextId: followId } });
+    },
+    [followLocal, followRemote, isLocal, hit, followId]
+  );
+
+  return useMemo(
+    () => {
+      return {
+        isFollowing,
+        isFollowable,
+        follow,
+        unfollow,
+        mutating,
+        isLocal
+      };
+    },
+    [isFollowing, isFollowable, follow, unfollow, mutating, isLocal]
+  );
+};
+
+export const isHitLocal = (hit: Hit, info: Q) => {
+  if (!info.instance) {
+    return null;
+  }
+  return hit.index_instance === info.instance.hostname;
+};
+
+export const hitFollowContextId = (hit: Hit, isLocal: boolean) => {
+  if (isLocal) {
+    return hit.index_instance_object_id;
+  } else {
+    return hit.canonicalUrl;
+  }
+};
+
+export const hitFollowId = (
+  hit: Hit,
+  followingCommunities: SearchFollowedCommunityFragment[],
+  followingCollections: SearchFollowedCollectionFragment[]
+) => {
+  if (hit.index_type === 'Community') {
+    const actor = followingCommunities.find(sfc => {
+      return (
+        sfc.community.canonicalUrl &&
+        hit.canonicalUrl &&
+        sfc.community.canonicalUrl === hit.canonicalUrl
+      );
+    });
+    return actor && actor.follow.id;
+  } else if (hit.index_type === 'Collection') {
+    const actor = followingCollections.find(sfc => {
+      return (
+        sfc.collection.canonicalUrl &&
+        hit.canonicalUrl &&
+        sfc.collection.canonicalUrl === hit.canonicalUrl
+      );
+    });
+    return actor && actor.follow.id;
+  } else {
+    return undefined;
+  }
+};
+export const getFollowingCollections = (
+  info: Q
+): SearchFollowedCollectionFragment[] => {
+  const followingCollectionsEdges =
+    info.me &&
+    info.me.user.followedCollections &&
+    info.me.user.followedCollections.edges;
+  if (!followingCollectionsEdges) {
+    return [];
+  }
+  const followingCollections = followingCollectionsEdges
+    .map(edge => edge && edge.node)
+    .filter((node): node is SearchFollowedCollectionFragment => !!node);
+  return followingCollections;
+};
+
+export const getFollowingCommunities = (
+  info: Q
+): SearchFollowedCommunityFragment[] => {
+  const followingCommunitiesEdges =
+    info.me &&
+    info.me.user.followedCommunities &&
+    info.me.user.followedCommunities.edges;
+  if (!followingCommunitiesEdges) {
+    return [];
+  }
+  const followingCommunities = followingCommunitiesEdges
+    .map(edge => edge && edge.node)
+    .filter((node): node is SearchFollowedCommunityFragment => !!node);
+  return followingCommunities;
+};
