8b92e4952e4ce699e8486de45e14b7f05318451e
aleclofabbro <alessandro.giansanti@gmail.com>
2020-02-07 10:19:11 +0100

    removed unused mocks ( dead code to  type-maintain ) moved some "common" stuff FormikHook type is manually extracted from lib type - try find a way to dynamically define it

--- src/util/ctx-mock/submitProviderActionDeco.tsx
+++ src/util/ctx-mock/submitProviderActionDeco.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Context } from 'react';
 import { action } from '@storybook/addon-actions';
-import { Service } from '../../types';
+export type Service<Request, Response> = (req: Request) => Promise<Response>;
 
 export const StorybookAsyncServiceMockProviderDeco = <Vals, Resp>(
   name: string,
