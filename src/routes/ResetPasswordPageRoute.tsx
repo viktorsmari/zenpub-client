c167faef0e0b0ae2ee70b7ab4026d1df2e169da7
aleclofabbro <alessandro.giansanti@gmail.com>
2020-04-20 12:10:43 +0200

    amend fix

--- src/routes/ResetPasswordPageRoute.tsx
+++ src/routes/ResetPasswordPageRoute.tsx
@@ -2,18 +2,15 @@ import React, { FC } from 'react';
 import { RouteComponentProps, RouteProps } from 'react-router-dom';
 import { GuestTemplate } from 'HOC/templates/Guest/Guest';
 import { ResetPasswordPageHOC } from 'HOC/pages/resetPasswordRequest/resetPasswordRequestPage';
-import { RedirectAuthenticatedToHome } from './wrappers/RedirectBySession';
 
 interface ResetPasswordPageRouter {}
 const ResetPasswordPageRouter: FC<RouteComponentProps<
   ResetPasswordPageRouter
 >> = ({ match }) => {
   return (
-    <RedirectAuthenticatedToHome>
-      <GuestTemplate>
-        <ResetPasswordPageHOC />
-      </GuestTemplate>
-    </RedirectAuthenticatedToHome>
+    <GuestTemplate>
+      <ResetPasswordPageHOC />
+    </GuestTemplate>
   );
 };
 
