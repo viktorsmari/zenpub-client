d94f5da5b831cfac9cd03bd79d54d6eeb2a518b6
aleclofabbro <alessandro.giansanti@gmail.com>
2020-04-20 12:07:23 +0200

    when resetting it auto logs in and redirect home

--- src/routes/ResetPasswordPageRoute.tsx
+++ src/routes/ResetPasswordPageRoute.tsx
@@ -2,15 +2,18 @@ import React, { FC } from 'react';
 import { RouteComponentProps, RouteProps } from 'react-router-dom';
 import { GuestTemplate } from 'HOC/templates/Guest/Guest';
 import { ResetPasswordPageHOC } from 'HOC/pages/resetPasswordRequest/resetPasswordRequestPage';
+import { RedirectAuthenticatedToHome } from './wrappers/RedirectBySession';
 
 interface ResetPasswordPageRouter {}
 const ResetPasswordPageRouter: FC<RouteComponentProps<
   ResetPasswordPageRouter
 >> = ({ match }) => {
   return (
-    <GuestTemplate>
-      <ResetPasswordPageHOC />
-    </GuestTemplate>
+    <RedirectAuthenticatedToHome>
+      <GuestTemplate>
+        <ResetPasswordPageHOC />
+      </GuestTemplate>
+    </RedirectAuthenticatedToHome>
   );
 };
 
