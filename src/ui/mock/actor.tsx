fa4f45df559104602bad42366f6f9d93e371d9ea
Doug Belshaw <doug@pop-os.localdomain>
2020-04-21 10:38:02 +0100

    Doug test

--- src/ui/mock/actor.tsx
+++ src/ui/mock/actor.tsx
@@ -2,5 +2,5 @@ export const getActor = () => ({
   icon:
     'https://pbs.twimg.com/profile_images/1161428802091802627/O49Ggs-7_400x400.jpg',
   link: '1',
-  name: '˗ˏˋ Doug Belshaw ˎˊ˗ '
+  name: 'Joe Bloggs'
 });
