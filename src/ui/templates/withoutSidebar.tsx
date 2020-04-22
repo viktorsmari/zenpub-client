7934c18e9b97dc8bead5f7fcd00e1500fc941bbd
aleclofabbro <alessandro.giansanti@gmail.com>
2020-04-22 17:05:52 +0200

    hooked to have a flag but  don't know how to use it

--- src/ui/templates/withoutSidebar.tsx
+++ src/ui/templates/withoutSidebar.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { ComponentType, useCallback, useState } from 'react';
 import { Flex } from 'rebass/styled-components';
 import styled from 'ui/themes/styled';
 
 export interface Props {
-  HeaderBox: JSX.Element;
+  HeaderBox: ComponentType<{ toggleSideBar(): unknown }>;
 }
-export const WithoutSidebar: React.FC<Props> = ({ children, HeaderBox }) => (
-  <Wrapper>
-    {HeaderBox}
-    <CenteredWrapper>
-      <Flex ml={2}>{children}</Flex>
-    </CenteredWrapper>
-  </Wrapper>
-);
+export const WithoutSidebar: React.FC<Props> = ({ children, HeaderBox }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSideBar = useCallback(() => setSidebarOpen(!isSidebarOpen), [
+    isSidebarOpen
+  ]);
+
+  return (
+    <Wrapper>
+      <HeaderBox toggleSideBar={toggleSideBar} />
+      <CenteredWrapper>
+        <Flex ml={2}>{children}</Flex>
+      </CenteredWrapper>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled(Flex)`
   justify-content: center;
