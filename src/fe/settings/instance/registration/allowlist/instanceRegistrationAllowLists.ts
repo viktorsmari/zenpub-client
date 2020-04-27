e5021cedc46b04dc8b8e564981ddbc966594368e
aleclofabbro <alessandro.giansanti@gmail.com>
2020-04-27 17:40:35 +0200

    removed pagination from instance whitelist, for now

--- src/fe/settings/instance/registration/allowlist/instanceRegistrationAllowLists.ts
+++ src/fe/settings/instance/registration/allowlist/instanceRegistrationAllowLists.ts
@@ -1,7 +1,7 @@
 import * as GQL from './instanceRegistrationAllowLists.generated';
 import { useMemo, useCallback } from 'react';
 import { usePage } from 'fe/lib/helpers/usePage';
-import { DEFAULT_PAGE_SIZE } from 'mn-constants';
+// import { DEFAULT_PAGE_SIZE } from 'mn-constants';
 
 export const useInstanceRegistrationAllowLists = () => {
   const [
@@ -21,12 +21,16 @@ export const useInstanceRegistrationAllowLists = () => {
   ] = GQL.useSendInviteEmailMutation();
 
   const listEmailsQ = GQL.useInstanceRegisterEmailAccessesQuery({
-    variables: { limit: DEFAULT_PAGE_SIZE }
+    variables: {
+      /* limit: DEFAULT_PAGE_SIZE  */
+    }
   });
   const listEmailsPage = usePage(listEmailsQ.data?.registerEmailAccesses);
 
   const listEmailDomainsQ = GQL.useInstanceRegisterEmailDomainAccessesQuery({
-    variables: { limit: DEFAULT_PAGE_SIZE }
+    variables: {
+      /* limit: DEFAULT_PAGE_SIZE  */
+    }
   });
   const listEmailDomainsPage = usePage(
     listEmailDomainsQ.data?.registerEmailDomainAccesses
