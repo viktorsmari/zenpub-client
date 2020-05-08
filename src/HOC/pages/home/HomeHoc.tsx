2d3ba00a0d9e9465aaaa5ba441e3f0900c5794e4
bernini <bernini@inventati.org>
2020-05-08 15:44:46 +0200

    commented followedComm and followedColl in home page

--- src/HOC/pages/home/HomeHoc.tsx
+++ src/HOC/pages/home/HomeHoc.tsx
@@ -1,14 +1,14 @@
 import { useMyInboxActivities } from 'fe/activities/inbox/my/useMyInboxActivities';
-import { useMyFollowedCollections } from 'fe/collection/myFollowed/myFollowedCollections';
-import { useMyFollowedCommunities } from 'fe/community/myFollowed/myFollowedCommunities';
+// import { useMyFollowedCollections } from 'fe/collection/myFollowed/myFollowedCollections';
+// import { useMyFollowedCommunities } from 'fe/community/myFollowed/myFollowedCommunities';
 import { useFormikPage } from 'fe/lib/helpers/usePage';
 import { ActivityPreviewHOC } from 'HOC/modules/previews/activity/ActivityPreview';
-import { CollectionPreviewHOC } from 'HOC/modules/previews/collection/CollectionPreview';
-import { CommunityPreviewHOC } from 'HOC/modules/previews/community/CommunityPreview';
+// import { CollectionPreviewHOC } from 'HOC/modules/previews/collection/CollectionPreview';
+// import { CommunityPreviewHOC } from 'HOC/modules/previews/community/CommunityPreview';
 import React, { FC, useMemo } from 'react';
 import { Home, Props } from 'ui/pages/home';
-import { MyFollowedCommunityDataFragment } from 'fe/community/myFollowed/myFollowedCommunities.generated';
-import { MyFollowedCollectionDataFragment } from 'fe/collection/myFollowed/myFollowedCollections.generated';
+// import { MyFollowedCommunityDataFragment } from 'fe/community/myFollowed/myFollowedCommunities.generated';
+// import { MyFollowedCollectionDataFragment } from 'fe/collection/myFollowed/myFollowedCollections.generated';
 export { HomePageTab } from 'ui/pages/home';
 
 export interface HomePageHOC {
@@ -16,53 +16,53 @@ export interface HomePageHOC {
 }
 
 export const HomePageHOC: FC<HomePageHOC> = ({ basePath }) => {
-  const { myCommunityFollowsPage } = useMyFollowedCommunities();
-  const [nextCommunitiesFormik] = useFormikPage(myCommunityFollowsPage);
-  const FollowedCommunitiesElements = useMemo<
-    Props['FollowedCommunitiesElements']
-  >(() => {
-    return (
-      <>
-        {myCommunityFollowsPage
-          .edges!.map(follow => follow.context)
-          .filter(
-            (context): context is MyFollowedCommunityDataFragment =>
-              context.__typename === 'Community'
-          )
-          .map(community => (
-            <CommunityPreviewHOC
-              communityId={community.id}
-              key={community.id}
-            />
-          ))}
-      </>
-    );
-  }, [myCommunityFollowsPage]);
+  // const { myCommunityFollowsPage } = useMyFollowedCommunities();
+  // const [nextCommunitiesFormik] = useFormikPage(myCommunityFollowsPage);
+  // const FollowedCommunitiesElements = useMemo<
+  //   Props['FollowedCommunitiesElements']
+  // >(() => {
+  //   return (
+  //     <>
+  //       {myCommunityFollowsPage
+  //         .edges!.map(follow => follow.context)
+  //         .filter(
+  //           (context): context is MyFollowedCommunityDataFragment =>
+  //             context.__typename === 'Community'
+  //         )
+  //         .map(community => (
+  //           <CommunityPreviewHOC
+  //             communityId={community.id}
+  //             key={community.id}
+  //           />
+  //         ))}
+  //     </>
+  //   );
+  // }, [myCommunityFollowsPage]);
 
-  const {
-    myCollectionFollowsPage: myFollowedCollectionsPage
-  } = useMyFollowedCollections();
-  const [nextCollectionsFormik] = useFormikPage(myFollowedCollectionsPage);
-  const FollowedCollectionsElements = useMemo<
-    Props['FollowedCollectionsElements']
-  >(() => {
-    return (
-      <>
-        {myFollowedCollectionsPage
-          .edges!.map(follow => follow.context)
-          .filter(
-            (context): context is MyFollowedCollectionDataFragment =>
-              context.__typename === 'Collection'
-          )
-          .map(collection => (
-            <CollectionPreviewHOC
-              collectionId={collection.id}
-              key={collection.id}
-            />
-          ))}
-      </>
-    );
-  }, [myFollowedCollectionsPage]);
+  // const {
+  //   myCollectionFollowsPage: myFollowedCollectionsPage
+  // } = useMyFollowedCollections();
+  // const [nextCollectionsFormik] = useFormikPage(myFollowedCollectionsPage);
+  // const FollowedCollectionsElements = useMemo<
+  //   Props['FollowedCollectionsElements']
+  // >(() => {
+  //   return (
+  //     <>
+  //       {myFollowedCollectionsPage
+  //         .edges!.map(follow => follow.context)
+  //         .filter(
+  //           (context): context is MyFollowedCollectionDataFragment =>
+  //             context.__typename === 'Collection'
+  //         )
+  //         .map(collection => (
+  //           <CollectionPreviewHOC
+  //             collectionId={collection.id}
+  //             key={collection.id}
+  //           />
+  //         ))}
+  //     </>
+  //   );
+  // }, [myFollowedCollectionsPage]);
 
   const { activitiesPage } = useMyInboxActivities();
   const [nextInboxFormik] = useFormikPage(activitiesPage);
@@ -85,20 +85,20 @@ export const HomePageHOC: FC<HomePageHOC> = ({ basePath }) => {
   const homeProps = useMemo<Props>(() => {
     const props: Props = {
       InboxElements,
-      FollowedCollectionsElements,
-      FollowedCommunitiesElements,
-      nextCollectionsFormik,
-      nextCommunitiesFormik,
+      // FollowedCollectionsElements,
+      // FollowedCommunitiesElements,
+      // nextCollectionsFormik,
+      // nextCommunitiesFormik,
       nextInboxFormik,
       basePath
     };
     return props;
   }, [
     InboxElements,
-    FollowedCollectionsElements,
-    FollowedCommunitiesElements,
-    nextCollectionsFormik,
-    nextCommunitiesFormik,
+    // FollowedCollectionsElements,
+    // FollowedCommunitiesElements,
+    // nextCollectionsFormik,
+    // nextCommunitiesFormik,
     nextInboxFormik,
     basePath
   ]);
