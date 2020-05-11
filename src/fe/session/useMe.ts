3e9586426391a84a597693ec2a9daca9dd8f1afd
aleclofabbro <alessandro.giansanti@gmail.com>
2020-05-11 10:08:48 +0200

    a logout action now brings user to login page

--- src/fe/session/useMe.ts
+++ src/fe/session/useMe.ts
@@ -1,11 +1,12 @@
 import { useMemo, useCallback } from 'react';
 import { mnCtx } from 'fe/lib/graphql/ctx';
 import * as GQL from './me.generated';
+import { useHistory } from 'react-router-dom';
 
 export const useMe = () => {
   const meQ = GQL.useMeQuery({ context: mnCtx({ noShowError: true }) });
   const [logoutMut, logoutStatus] = GQL.useMeLogoutMutation();
-
+  const { push } = useHistory();
   const me = meQ.data?.me;
   const loading = meQ.loading;
   const isAdmin = !!me?.isInstanceAdmin;
@@ -23,7 +24,7 @@ export const useMe = () => {
           query: GQL.MeDocument
         });
       }
-    });
+    }).finally(() => push('/login'));
   }, [me, logoutStatus.loading]);
 
   return useMemo(() => {
